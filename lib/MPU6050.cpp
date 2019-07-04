722ca7c5f0b6ee66a321ae116c718ef2c6ca06bb
Youri <youridv1@gmail.com>
2019-07-04 15:35:07 +0200

    second commit

--- lib/MPU6050.cpp
+++ lib/MPU6050.cpp
@@ -1,3 +1,9 @@
+
+//          Copyright Youri de Vor 2018 - 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #include "MPU6050.hpp"
 #include "hwlib.hpp"
 #include <cmath>
--- lib/stepper28BYJ48.cpp
+++ lib/stepper28BYJ48.cpp
@@ -1,3 +1,8 @@
+//          Copyright Youri de Vor 2018 - 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #include "stepper28BYJ48.hpp"
 
 stepper28BYJ48::stepper28BYJ48(hwlib::port_out & port, uint16_t stepsPerRotation):
